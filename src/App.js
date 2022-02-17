08aaf1714cc5871dc5d3bb6e12494d45bf27617f
hahriar <closebrocircle@gmail.com>
2022-02-17 08:21:47 +0600

    third Commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Countries></Countries>
+      
     </div>
   );
 }
-function Countries(){
+
+/* function Countries(){
   const [countries , setCountries] = useState([]);
   useEffect( () =>{
     fetch('https://restcountries.com/v2/all')
@@ -20,8 +20,20 @@ function Countries(){
     <div>
       <h1>Traveling Around The World!</h1>
       <h4> Countries Available: {countries.length}</h4>
+      {
+        countries.map(country => <Country name ={country.name} capital={country.capital}></Country>)
+      }
     </div>
   )
 }
+
+function Country(props){
+  return(
+    <div>
+        <h2> Name: {props.name}</h2>
+        <p> Capital: {props.capital}</p>
+    </div>
+  )
+} */
 
 export default App;

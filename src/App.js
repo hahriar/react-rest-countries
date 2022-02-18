1774a267bf428e45469afb9bc8b7af51fe53ca79
hahriar <closebrocircle@gmail.com>
2022-02-18 17:21:00 +0600

    forth commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import { useEffect, useState } from 'react';
+import Countries from './components/Countries/Countries';
 
 function App() {
   return (
     <div className="App">
-      
+      <Countries></Countries>
     </div>
   );
 }
--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Countries = () => {
+    const [countries , setCountries] = useState([])
+
+    useEffect( () =>{
+        fetch('https://restcountries.com/v2/all')
+        .then(res => res.json())
+        .then(data => setCountries(data))
+    }, [])
     return (
         <div>
-            
+            <h1>Hello From The Other Side : {countries.length}</h1>
         </div>
     );
 };
